e0630229fc90265a8722e392c68e7a7d6c1542a0
ppbao <bruce.liu.02@gmail.com>
2017-03-02 11:11:04 +1000

    add database and configuration

---
+++ app/src/main/java/au/com/unisharing/askweather/db/Province.java
@@ -0,0 +1,30 @@
+package au.com.unisharing.askweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
